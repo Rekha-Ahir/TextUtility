368f67eedb8b1fd4591ef6b994b8d0489995e427
Rekha-Ahir <REKHAAHIR2002@GMAIL.COM>
2023-10-11 08:36:18 -0700

    text complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import TextForm from './Components/TextForm';
+
+import Navbar from './Components/Navbar';
+import Alert from './Components/Alert';
+import { useState } from 'react';
 
 function App() {
-  return (
+  const[mode,setMode]=useState('light')
+  const[alert,showAlert]=useState('')
+  const toggleMode=()=>{
+  if(mode === 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor = '#042743';
+    document.body.style.color='white';
+    showAlert("Dark mode has been enabled", "success");
+    document.title = 'TextUtils - Dark Mode';
+   
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color='black';
+    showAlert("Light mode has been enabled", "success");
+    document.title = 'TextUtils - Light Mode';
+  }}
+  
+  return (<>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar  mode ={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      < TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert}/>
     </div>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+const Navbar = (props) => {
+  return (
+    <>
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">TextUtility</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+        <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                    </div>
+      </nav>
+    </>
+  )
+}
+export default Navbar;
